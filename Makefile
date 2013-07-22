39783e63042341c703ec9ee5132182b2db81cb74
root <root@localhost.localdomain>
2013-07-22 18:42:06 -0400

    added infinite loop to the server ctrl+c to quit

--- Makefile
+++ Makefile
@@ -1,23 +1,23 @@
 ##
 ## Makefile
 ##
+## submitted by Matt Cooper mxc5178@rit.edu
 ##
 
 CC = g++ -g -Wno-deprecated
 CFLAGS = ''
 
-client :
-    $(CC) -o smtp-client client.cpp
+smtp-server :
+	$(CC) -o smtp-server server.cpp
 
-server :
-    $(CC) -o smtp-server server.cpp
+clean-server :
+	rm -f smtp-server
 
 example-server :
-    $(CC) -o example-server example-server.cpp
+	$(CC) -o example-server example-server.cpp
 
 example-client :
-    $(CC) -o example-client example-client.cpp
+	$(CC) -o example-client example-client.cpp
 
-clean :
-    rm -f smtp-client && rm -f smtp-server && rm -f example-server rm -f example-client
-
+clean-example :
+	rm -f example-server && rm -f example-client
--- server.cpp
+++ server.cpp
@@ -1,3 +1,191 @@
-##
-## main function for server will go here
-##
+//
+// simple SMTP server
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+//
+// function to print an error message and exit the program
+//
+void error(const char *msg){
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[]){
+
+    //
+    // check that there are two command line arguments ... first
+    //    arg[1] is the port number
+    //    for example, in a Linux/Mac/Unix terminal window
+    //      ./smtp-server 31000
+    //  
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+
+    //
+    // sockfd and newsockfd -> sockets
+    // portno -> port number to listen on
+    //
+    int sockfd, newsockfd, portno;
+
+    //
+    // client_len is a variable to hold the size(in bytes) of 
+    // a sockaddr_in struct  
+    // 
+    socklen_t client_len;
+
+    //
+    // buffer is an array of chars that will hold the message
+    // being received from a client or sent to a client    
+    // 
+    char buffer[256];
+
+    //
+    // serv_addr: variable of type sockaddr_in
+    //    serv_addr is a struct which holds the Ip
+    //    information for the server
+    // 
+    // cli_addr: variable of type sockaddr_in
+    //    cli_addr is a struct which holds the Ip
+    //    information for the client once a client connection
+    //    has been accepted by this server 
+    //
+    // cli_addr will be compared to serv_addr to determine that
+    //     the client and server are on the same network
+    // 
+    struct sockaddr_in serv_addr, cli_addr;
+
+    //
+    // use n to read from and write to the sockets
+    //
+    int n;
+
+    //
+    // create a socket using the socket function; the value
+    // returned by the socket() function is a handle to the socket
+    // (similar to the descriptor of a file)
+    // save the socket handle; it will be needed by the bind(), listen(), and accept() functions
+    //
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //
+    // if socket() returns a negative number, there has been an error 
+    //
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+
+    //
+    // bzero() is a function that fills a block of memory locations with binary zero
+    //  the following call to bzero sets the location occupied by the
+    //  serv_addr struct with all zeroes.
+    //
+    // bzero just clears the memory location to remove any prior data that is unwanted  
+    //
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    //
+    // convert the alpha port number from the command line to a number 
+    //
+    portno = atoi(argv[1]);
+
+    //
+    // fill the serv_addr struct members with appropriate information 
+    //
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    //
+    // Bind the socket to a specific port using the socket handle and the serv_addr struct 
+    //
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        error("ERROR on binding");
+
+    //
+    // loop forever ... it's a server
+    //
+    while(1){
+
+        //
+        // Listen on the socket for a maximum number of connections; in this example
+        //    max connections is 5 but the number is arbitrary and meaningless because
+        //    this is a single threaded application
+        //
+        listen(sockfd,5);
+
+        //
+        // get the size of the cii_addr struct 
+        //
+        client_len = sizeof(cli_addr);
+        
+        // 
+        // accept connections from the socket; accept returns a new socket descriptor which
+        //    is a new socket associated with the client requesting the connection;  
+        //    the accept function fills the cli_addr struct with the address information
+        //    about the client; communication with the client now occurs on the new socket
+        //    see the read and write functions below    
+        //
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &client_len);
+        if (newsockfd < 0) 
+            error("ERROR on accept");
+
+
+        //
+        // clear the buffer
+        //
+        bzero(buffer,256);
+
+        //
+        // get the message from the client
+        //
+        // consider changing read() to recv()
+        //
+        n = read(newsockfd,buffer,255);
+
+        //
+        // check for specific messages here:
+        //  HELO
+        //  MAIL FROM:
+        //  RCPT TO:
+        //  DATA
+        //  QUIT
+        //
+
+        if (n < 0) error("ERROR reading from socket");
+        printf("Here is the message: %s\n",buffer);
+ 
+        //
+        // send response to the client:
+        //  220 - Hello request
+        //  221 - Goodbye
+        //  250 - Ok
+        //  354 - Send your message
+        //  501 - Syntax error
+        //  55x - Failure
+        //
+        // consider changing write() to send()
+        //
+        n = write(newsockfd,"I got your message",18);
+        if (n < 0) error("ERROR writing to socket");
+
+    }
+    //
+    // end forever loop
+    //
+
+    //
+    // close both sockets and end the program
+    //
+    close(newsockfd);
+    close(sockfd);
+    return 0; 
+}
